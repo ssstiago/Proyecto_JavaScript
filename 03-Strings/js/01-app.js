c54af17db8c8d44f55087cddc11b88a88951d72c
Saul Santiago Sanchez <Saulchez36@gmail.com>
2021-04-29 08:30:14 -0500

    Complemento Strings

---
+++ 03-Strings/js/01-app.js
@@ -0,0 +1,7 @@
+const prodcuto = 'Monitor 20 pulgadas';
+const producto2 = String('Monitor 24 pulgadas');
+const prodcuto3 = new String('Monitor 27 Pulgadas');
+
+console.log(prodcuto);
+console.log(producto2);
+console.log(prodcuto3);
