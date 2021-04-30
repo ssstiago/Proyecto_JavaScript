0c04dc65893af814950ad12a5e61c62baf2ea5c0
Saul Santiago Sanchez <Saulchez36@gmail.com>
2021-04-30 10:12:00 -0500

    Terminando Seccion Operadores

---
+++ 05-Operadores/js/02-app.js
@@ -0,0 +1,24 @@
+// Operadores de Comparacion 
+
+const numero1 = 20;
+const numero2 = '20';
+const numero3 = 30;
+
+// Revisar dos numeros iguales
+
+console.log(numero1 == numero3);
+console.log(numero1 == numero2);
+
+// Comparador estricto 
+console.log(numero1 === numero2);
+console.log(numero1 === numero3);
+console.log(typeof numero1);
+console.log(typeof numero2);
+
+// Comparador de diferencia
+const password1 = 'admin';
+const password2 = 'Admin';
+
+console.log(password1 != password2);
+console.log(numero1 != numero2);
+console.log(numero1 != parseInt(numero2));
---
+++ 05-Operadores/js/03-app.js
@@ -0,0 +1,12 @@
+// Undefined
+let numero;
+console.log(numero);
+
+console.log(typeof numero);
+
+//null
+let numero2 = null;
+console.log(numero2);
+// console.log(typeof numero2);
+
+console.log(numero === numero2);
