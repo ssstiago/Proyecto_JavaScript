6f73b1b5d9597956c2c33626449638570681890d
Saul Santiago Sanchez <Saulchez36@gmail.com>
2021-05-09 16:35:21 -0500

    Comienzo Curso _Arrays

---
+++ 08-Arrays/js/01-app.js
@@ -0,0 +1,14 @@
+// Concepto de Arraays
+const numeros = [10, 20, 30];
+
+
+// Otra forma de hacer arrays
+const meses = new Array('Enero', 'Febrero', 'Marzo');
+
+console.log(numeros);
+console.log(meses);
+
+
+// Cracion de arreglod con datos de todo tipo
+const deTodo = ['Hola', 10, true, 'si', null, { nombre: 'Santiago', trbajo: 'Programador' }];
+console.log(deTodo);
